b4dcc018b136596b9918bae8d8e42b303fdd0529
AlexJamesWright <a.j.wright@soton.ac.uk>
2021-08-07 13:23:26 +0100

    packagedir retry

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='PokerTable',
       version='0.1',
@@ -6,8 +6,6 @@ setup(name='PokerTable',
       url='https://github.com/AlexJamesWright/PokerTable',
       author='Alex James Wright',
       author_email='alex.j.wright2@gmail.com',
-      packages=['pokertable'],
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='nose.collector',
-      tests_require=['nose'])
+      packages=find_packages(),
+      package_dir = {"" : "./"},
+      include_package_data=True)
